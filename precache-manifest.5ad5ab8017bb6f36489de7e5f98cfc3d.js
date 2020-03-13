a364128d6251547596ca526c99c7beb7ce8b0b62
Ali Bayramli <alibayramli97@gmail.com>
2020-03-13 01:19:39 +0100

    Updates

---
+++ precache-manifest.5ad5ab8017bb6f36489de7e5f98cfc3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b20fcdeb77344103a23f3c131aa1b7",
+    "url": "/calorie-tracker/index.html"
+  },
+  {
+    "revision": "0b84b8e6ca54dc475425",
+    "url": "/calorie-tracker/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d7dbd0bb032116c809bd",
+    "url": "/calorie-tracker/static/css/main.62d56984.chunk.css"
+  },
+  {
+    "revision": "0b84b8e6ca54dc475425",
+    "url": "/calorie-tracker/static/js/2.dcc45107.chunk.js"
+  },
+  {
+    "revision": "95fe7b47190d8ffb46ace11b00922862",
+    "url": "/calorie-tracker/static/js/2.dcc45107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7dbd0bb032116c809bd",
+    "url": "/calorie-tracker/static/js/main.6a09cb5a.chunk.js"
+  },
+  {
+    "revision": "c4998910e13a17317c21",
+    "url": "/calorie-tracker/static/js/runtime-main.870f7726.js"
+  }
+]);
