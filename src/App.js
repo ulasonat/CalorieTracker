a07a9b0ccadb56447748c7a40a489923bebf75ef
ipl4dc <ipl4dc@inf.elte.hu>
2020-03-17 22:27:09 +0100

    Added the basic classs structure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React from "react";
 import Nav from "./components/Nav";
 import Form from "./components/Form";
 import "./App.css";
+
+//This class will contain the Navigation Bar
+class NavBar extends React.Component{
+}
+
+//this class will be a sidebar that contais food items that were added in the current session
+class RecentlyEntered extends React.Component{
+}
+
+//This class will be a sidebar that contains the food items that were added in older sessions(this will be a historylike feature )
+class RecentlHistory extends React.Component{
+}
+
+//This will be the SearchBar
+class SearchBar extends React.Component{
+
+}
+
+
 class App extends React.Component {
   constructor() {
     super();
@@ -12,10 +31,10 @@ class App extends React.Component {
       <div className="App">
         <Nav />
         <header className="App-header">
-          <p>
-            Hey <b>[user], </b>here is how to track your calorie consumption:{" "}
-          </p>
-          <Form />
+        <p>
+          Hey <b>[user], </b>here is how to track your calorie consumption:
+        </p>
+        <Form/>
         </header>
       </div>
     );
