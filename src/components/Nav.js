b6190c8ac474504721473ed247e914d002f06106
Ali Bayramli <alibayramli97@gmail.com>
2020-03-23 23:19:15 +0100

    Change in the style of navigation bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,20 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 export default function Nav() {
-  return <></>;
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Calorie Counter
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
 }
