8fc47f0348254ee0385b43ccfcfe249736a0027c
Ali Bayramli <alibayramli97@gmail.com>
2020-05-21 21:00:54 +0200

    Checking if cd works as expected

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Nav from "./components/Nav";
 import "./App.css";
 import Body from "./components/Body";
-function App(){
-    return (
-      <div>
-        <Nav />
-        <Body />
-      </div>
-    )
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Body />
+      {console.log("cd works as expected!")}
+    </div>
+  )
 }
 
 
